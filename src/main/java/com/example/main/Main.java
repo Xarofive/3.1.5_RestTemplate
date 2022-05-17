f71359ae714ff7bd1bd9476b7016801e67c722d3
Denis Pronin <de.pronin@gmail.com>
2022-05-17 19:24:02 +0300

    рефакторинг

--- src/main/java/com/example/main/Main.java
+++ src/main/java/com/example/main/Main.java
@@ -12,38 +12,33 @@ public class Main {
     public static final String URL = "http://94.198.50.185:7081/api/users";
 
     public static void main(String[] args) {
-
         RestTemplate restTemplate = new RestTemplate();
-        User user = new User(3L, "James", "Brown", (byte) 20);
 
         HttpHeaders headers = new HttpHeaders();
-
-        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<String> resultGet
+        // шаг 1
+        ResponseEntity<String> response
                 = restTemplate.exchange(URL, HttpMethod.GET, entity, String.class);
-
-        headers.set(HttpHeaders.COOKIE, resultGet.getHeaders().getFirst(HttpHeaders.SET_COOKIE));
-
-
+        // шаг 2
+        headers.set(HttpHeaders.COOKIE, response.getHeaders().getFirst(HttpHeaders.SET_COOKIE));
+        // шаг 3
+        User user = new User(3L, "James", "Brown", (byte) 20);
         HttpEntity<User> requestBody = new HttpEntity<>(user, headers);
         ResponseEntity<String> resultSave
                 = restTemplate.exchange(URL, HttpMethod.POST, requestBody, String.class);
-
-
+        // шаг 4
         user.setName("Thomas");
         user.setLastName("Shelby");
         requestBody = new HttpEntity<>(user, headers);
         ResponseEntity<String> resultUpdate
                 = restTemplate.exchange(URL, HttpMethod.PUT, requestBody, String.class);
-
+        // шаг 5
         requestBody = new HttpEntity<>(headers);
         ResponseEntity<String> resultDelete
                 = restTemplate.exchange(URL + "/" + user.getId(), HttpMethod.DELETE, requestBody, String.class);
-
+        // результат
         System.out.print(resultSave.getBody());
         System.out.print(resultUpdate.getBody());
         System.out.print(resultDelete.getBody());
